4f2140349d7f8caba84491de3cc096fe00bd808b
Hamza Kadiri <hamza.kadiri@student.ecp.fr>
2018-11-25 18:17:36 +0100

    First view

--- src/index.php
+++ src/index.php
@@ -1,4 +1,26 @@
+<?php
+$opts = ["http" => ["method" => "GET", "header" => "User-Agent: hamza-kadiri"]];
+$context = stream_context_create($opts);
+$url = 'https://api.github.com/repos/torvalds/linux/commits';
+$data = file_get_contents($url, false, $context);
+$json = json_decode($data, true); 
 
+$format_commits = function ($commit)
+{
+  return array(
+    "sha" => $commit["sha"],
+    "committer_name" => $commit["commit"]["committer"]["name"],
+    "committer_url" => $commit["committer"]["html_url"],
+    "date" => $commit["commit"]["committer"]["date"],
+    "message" => $commit["commit"]["message"],
+    "login" => $commit["committer"]["login"],
+    "image" => $commit["committer"]["avatar_url"]
+  );
+};
+
+$commits = array_map($format_commits, $json);
+
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,8 +29,52 @@
     <title>Github API</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.2/css/bulma.min.css">
 </head>
 <body>
-<?php echo 'Hello World' ?>
+<body>
+  <section class="section">
+    <div class="container">
+      <h1 class="title">
+        Github commits
+      </h1>
+      <p class="subtitle">
+        Made by <strong>Hamza Kadiri</strong>
+      </p>
+    <div>
+  </section>
+  <section class="section">
+    <div class="container commits">
+        <?php foreach ($commits as $commit) { ?>
+            <article class="media">
+                <figure class="media-left">
+                    <p class="image is-96x96">
+                        <a href=<?php echo $commit['committer_url']; ?>>
+                            <img src=<?php echo $commit["image"]; ?>>
+                        </a>
+                    </p>
+                </figure>
+                <div class="media-content">
+                    <div class="content">
+                        <p> <strong> <?php echo $commit["committer_name"]; ?></strong> 
+                            <small>@<?php echo $commit["login"]; ?></small>
+                            <small> committed at <?php echo date_format(date_create($commit["date"]) , "Y-m-d H:i:s"); ?> </small>
+                            <br/>
+                            <?php if (strlen($commit["message"]) <= 300) {
+                                    echo $commit["message"]; 
+                                    }
+                                else {
+                                    echo substr($commit["message"], 0, 300) . " ..."; 
+                                    } ?>
+                        </p>
+                    </div>
+                </div>
+                <div class="media-right">
+                    <span><?php echo substr($commit["sha"], 0, 12); ?></span>
+                </div>
+            </article>
+      <?php } ?>
+    </div>
+  </section>
 </body>
 </html>
